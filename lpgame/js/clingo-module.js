628a1d5fdcfc22b7d00b736aed5cf971a74161f9
Ronald de Haan <me@ronalddehaan.eu>
2024-10-29 15:00:57 +0100

    Add test

---
+++ lpgame/js/clingo-module.js
@@ -0,0 +1,104 @@
+// This file is released under the MIT license.
+// See LICENSE.md.
+//
+// The code in this file is taken (in modified form) from:
+// https://github.com/potassco/potassco.github.io
+
+var ClingoModule = {};
+
+var stored_programs = {}
+function save_stored_program(name, program) {
+    stored_programs[name] = program;
+}
+function load_named_program(name, path) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        program = request.responseText.trim()
+        save_stored_program(name, program);
+      }
+    }
+    request.open("GET", path, true);
+    request.send();
+}
+// load_named_program("heuristics", "stored/heuristics.lp")
+
+var output_elem = document.getElementById('output');
+var output = "";
+
+function clearOutput() {
+  output = "";
+  updateOutput();
+}
+
+function addToOutput(text) {
+  if (output == "") {
+      output += text;
+  } else {
+      output += "\n" + text;
+  }
+  updateOutput();
+}
+
+function updateOutput() {
+  if (output_elem) {
+    output_elem.textContent = output;
+    outputElement.scrollTop = outputElement.scrollHeight; // focus on bottom
+  }
+}
+
+function solve() {
+
+  output = "";
+
+  options = "-n1 -Wnone";
+  program = "a :- not b.\nb :- not a.\n"
+  ClingoModule.ccall('run', 'number', ['string', 'string'], [program, options])
+
+  updateOutput();
+  // document.getElementById("run").disabled = false;
+}
+
+function handleOutputLine(text) {
+  addToOutput(text);
+  updateOutput();
+}
+
+const version = '0.3.0';
+
+d3.require(`wasm-clingo@${version}`).then(Clingo => {
+    const Module = {
+        locateFile: (file) => `https://cdn.jsdelivr.net/npm/wasm-clingo@${version}/${file}`,
+        print: function(text) {
+            handleOutputLine(text);
+        },
+        printErr: function(err) {
+            Module.setStatus('Error')
+            console.error(err)
+        },
+        setStatus: function(text) {
+            output_elem.innerHTML += text + "<br>";
+        },
+        totalDependencies: 0,
+        monitorRunDependencies: function(left) {
+            this.totalDependencies = Math.max(this.totalDependencies, left);
+            Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
+        }
+    }
+
+    window.onerror = function(event) {
+        Module.setStatus('Exception thrown, see JavaScript console');
+        Module.setStatus = function(text) {
+            if (text) Module.printErr('[post-exception status] ' + text);
+        };
+    };
+    Module.setStatus('Downloading...');
+
+    Clingo(Module).then(clingo => {
+        ClingoModule = clingo;
+        document.getElementById("run").disabled = false;
+    });
+
+});
+
+handleOutputLine("")
---
+++ lpgame/js/control.js
@@ -0,0 +1,85 @@
+var level_state = "";
+var level_settings = "";
+
+function load_game() {
+  load_game_from_path(example_games.value);
+  load_status.innerText = "Loading successful.."
+}
+
+function load_game_from_path(path) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      game = JSON.parse(request.responseText.trim())
+      level_gen_program.setValue(game['level_gen_program'], 1);
+      visibility_program.setValue(game['visibility_program'], 1);
+      player_plan_program.setValue(game['player_plan_program'], 1);
+      player_move_program.setValue(game['player_move_program'], 1);
+      nature_program.setValue(game['nature_program'], 1);
+      goal_program.setValue(game['goal_program'], 1);
+      level_state = game['level_state'];
+      level_settings = game['level_settings'];
+    }
+  }
+  request.open("GET", path, true);
+  request.send();
+}
+
+file_input = document.getElementById('file_input');
+upload_status = document.getElementById('upload_status');
+example_games = document.getElementById('example_games');
+
+function change_tabs() {
+  level_gen_program.resize();
+  visibility_program.resize();
+  player_plan_program.resize();
+  player_move_program.resize();
+  nature_program.resize();
+  goal_program.resize();
+  upload_status.innerText = ""
+  load_status.innerText = ""
+}
+
+async function load_game_from_file(event) {
+  const file = event.target.files.item(0)
+  const text = await file.text();
+  game = JSON.parse(text);
+  level_gen_program.setValue(game['level_gen_program'], 1);
+  visibility_program.setValue(game['visibility_program'], 1);
+  player_plan_program.setValue(game['player_plan_program'], 1);
+  player_move_program.setValue(game['player_move_program'], 1);
+  nature_program.setValue(game['nature_program'], 1);
+  goal_program.setValue(game['goal_program'], 1);
+  upload_status.innerText = "Loading successful.."
+}
+
+function download_game() {
+  var game = {}
+  game['level_gen_program'] = level_gen_program.getValue();
+  game['visibility_program'] = visibility_program.getValue();
+  game['player_plan_program'] = player_plan_program.getValue();
+  game['player_move_program'] = player_move_program.getValue();
+  game['nature_program'] = nature_program.getValue();
+  game['goal_program'] = goal_program.getValue();
+  game['level_state'] = level_state;
+  game['level_settings'] = level_settings;
+  var text = JSON.stringify(game, null, 2);
+  save('game.json', text)
+}
+
+function save(filename, data) {
+    const blob = new Blob([data], {type: 'text/csv'});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        const elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;
+        document.body.appendChild(elem);
+        elem.click();
+        document.body.removeChild(elem);
+    }
+}
+
+// load_game_from_path('examples/flag1.json');
